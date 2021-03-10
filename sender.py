b137e725743f13f442cbaad9e9f81b7291b78f54
Rubikon23 <51418727+Rubikon23@users.noreply.github.com>
2021-03-10 16:44:37 +0300

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,17 +1,23 @@
 import requests
 from pyzabbix import ZabbixMetric, ZabbixSender
-
+import time
 
 def main():
-
-    url = 'http://web:5000/metrics'
-    response = requests.get(url)
-    metric = response.text.split()[-1]
-    forsend = []
-    m = ZabbixMetric('DevTest', 'Metric', metric)
-    forsend.append(m)
-    zbx = ZabbixSender('zabbix-server')
-    zbx.send(forsend)
+    while True:
+        visit = open("/code/visit", "r").read()
+        url = 'http://web:5000/metrics'
+        response = requests.get(url)
+        metric = response.text.split()[-1]
+        amount = int(metric) - int(visit)
+        forsend = []
+        m = ZabbixMetric('DevTest', 'Metric', amount)
+        forsend.append(m)
+        zbx = ZabbixSender('zabbix-server')
+        zbx.send(forsend)
+        refresh_visit = open("/code/visit", "w")
+        refresh_visit.write(metric)
+        refresh_visit.close()
+        time.sleep(1)
 
 
 if __name__ == '__main__':
